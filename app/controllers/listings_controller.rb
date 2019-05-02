ab9118d3f2942353d4e87d93a9f2ee210a688b1b
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-02 18:11:56 +0100

    Updated ctlr to allow users to create a new listing within a category

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -9,4 +9,22 @@ class ListingsController < ApplicationController
 
   end
 
+  def create
+
+    # Find the current category ID
+    @category = Category.find(params[:category_id])
+    @listing = @category.listings.new(form_params)
+
+    @listing.save
+
+    redirect_to category_path(@category)
+
+  end
+
+  def form_params
+
+    params.require(:listing).permit(:title, :body)
+
+  end
+
 end
