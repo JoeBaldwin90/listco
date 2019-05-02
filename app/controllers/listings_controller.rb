f26c4184e2a90914d2595a39769b41ea2b1c68e3
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-02 18:01:34 +0100

    Add form for creating a new listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,7 +2,9 @@ class ListingsController < ApplicationController
 
   def new
 
+    # Find the current category ID
     @category = Category.find(params[:category_id])
+    # New listing = Add new form data to the Listing model which belongs to the current category ID.
     @listing = @category.listings.new
 
   end
